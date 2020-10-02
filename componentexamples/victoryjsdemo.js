42f739d0a7910bcc6f68d4754d897ad439d913d4
Martin <marb61a@gmail.com>
2020-10-02 19:33:26 +0100

    Updated notes and added example victory js file

---
+++ componentexamples/victoryjsdemo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { VictoryBar, VictoryChart } from 'victory';
+
+const styles = {
+    fontFamily: 'sans-serif',
+    textAlign: "center"
+}
+
+const data = [
+
+];
