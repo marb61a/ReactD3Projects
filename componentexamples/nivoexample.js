7042c6becff954e96a5ac6ce7948e58e1a967569
Martin <marb61a@gmail.com>
2020-10-02 20:06:53 +0100

    Added example nivo js file

---
+++ componentexamples/nivoexample.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { ResponsiveBar } from '@nivo/bar';
+
+const styles = {
+    fontFamily: 'sans-serif',
+    textAlign: "center"
+}
+
+const data = [
+    { quarter: 1, earnings: 13000},
+    { quarter: 1, earnings: 16250},
+    { quarter: 1, earnings: 14500},
+    { quarter: 1, earnings: 19000}
+];
+
+const App = () => (
+    <div style={styles}>
+        <h1>Nivo Basic Demo</h1>
+        <div style={{ height: "400px" }}>
+            <ResponsiveBar data={data} keys={["earnings"]} indexBy="quarter"/>
+        </div>
+    </div>
+);
+
+render(
+    <App />, document.getElementById("root")
+);
