b76a7d40adb950094a668d9ab6cf8d4450e536e6
Martin <marb61a@gmail.com>
2020-10-05 11:42:43 +0100

    added axisreactexample and updated d3axiscode and notes file

---
+++ componentexamples/axisreactexample/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Axis from './axis';
+
+const styles = {
+    fontFamily: 'sans-serif',
+    textAlign: "center"
+}
+
+const App = () => (
+    <svg>
+
+    </svg>
+);
+
+render(
+    <App />, document.getElementById("root")
+);
--- componentexamples/d3axiscode.js
+++ componentexamples/d3axiscode.js
@@ -5,7 +5,15 @@ const scale = d3
     .domain([0, 10])
     .range([0, 200])
 
+// Will put axis on top bottom
 const axis = d3.axisBottom(scale);
+
+// Will put axis on top
+// const axis = d3.axisTop(scale);
+// Will put axis on left
+// const axis = d3.axisLeft(scale);
+// Will put axis on right
+// const axis = d3.axisRight(scale);
 
 d3.select("svg")
   .append("g")
