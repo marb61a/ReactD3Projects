a91f1a1e14f4df4cdedcc47939372908011ddb59
Martin <marb61a@gmail.com>
2020-10-06 19:28:03 +0100

    added hocexample files

---
+++ componentexamples/hocexample/axis.js
@@ -0,0 +1,8 @@
+import * as d3 from "d3";
+import D3Blackbox from './D3blackbox';
+
+const Axis = D3Blackbox(function() {
+
+});
+
+export default Axis;
---
+++ componentexamples/hocexample/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Axis from './axis';
+
+const styles = {
+    fontFamily: 'sans-serif',
+    textAlign: "center"
+}
+
+const App = () => (
+    <svg width="800" height="400" id="svg">
+        <Axis x={10} y={50}/>
+    </svg>
+);
+
+render(
+    <App />, document.getElementById("root")
+);
