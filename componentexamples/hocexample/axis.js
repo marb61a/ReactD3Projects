369ba3801f131546c07b21a1ca18d4de6e3ef0c7
Martin <marb61a@gmail.com>
2020-10-06 19:35:11 +0100

    Updated hocexample files

--- componentexamples/hocexample/axis.js
+++ componentexamples/hocexample/axis.js
@@ -2,7 +2,14 @@ import * as d3 from "d3";
 import D3Blackbox from './D3blackbox';
 
 const Axis = D3Blackbox(function() {
+    const scale = d3
+        .scaleLinear()
+        .domain([0, 10])
+        .range([0, 200]);
+    
+    const axis = d3.axisBottom(scale);
 
+    d3.select(this.anchor.current).call(axis);
 });
 
 export default Axis;
