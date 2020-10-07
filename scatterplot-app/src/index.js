2eb7cd52e2722a882d4d6dd85fddf8beae40d4de
Martin <marb61a@gmail.com>
2020-10-07 19:23:43 +0100

    added d3 and refactored index.js and updated app.js

--- scatterplot-app/src/index.js
+++ scatterplot-app/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as d3 from "d3";
+
+const data = d3
+  .range(100)
+  .map(_ => [
+    Math.random(),
+    Math.random()
+  ]);
+
+const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />, rootElement
 );
 
 // If you want your app to work offline and load faster, you can change
