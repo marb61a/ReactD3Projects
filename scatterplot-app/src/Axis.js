2e9cca5ee9ff10bf926299f6d364e6cd7007b899
Martin <marb61a@gmail.com>
2020-10-08 19:57:31 +0100

    Added dependencies and Axis.js file

---
+++ scatterplot-app/src/Axis.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as d3 from "d3";
+import styled from "styled-components";
+
+const Text = styled.text`
+  fill: black;
+  font-family: sans-serif;
+  font-size: 10px;
+`;
+
+class Axis extends React.Component{
+    constructor(){
+        super();
+        this.gRef = React.createRef();
+    }
+    
+}
+
+export default Axis;
