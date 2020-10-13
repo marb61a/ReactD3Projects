407aca307015e145b04df0ff506a0c2a8f138041
Martin <marb61a@gmail.com>
2020-10-13 19:49:51 +0100

    Updated hoc example to use react hook and updated notes

--- componentexamples/hocexample/axis.js
+++ componentexamples/hocexample/axis.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import { React, useRef, useEffect }from 'react';
 import * as d3 from "d3";
-import D3Blackbox from './D3blackbox';
+// import D3Blackbox from './D3blackbox';
 
 // const Axis = D3Blackbox(function() {
 //     const scale = d3
@@ -13,10 +13,25 @@ import D3Blackbox from './D3blackbox';
 //     d3.select(this.anchor.current).call(axis);
 // });
 
+// Will replace the commented function
 const Axis = ({ x, y }) => {
+    const gRef = useRef();
+
+    useEffect(() => {
+        const scale = d3
+            .scaleLinear()
+            .domain([0, 10])
+            .range([0, 200]);
+        
+        const axis = d3.axisBottom(scale);
+
+        d3.select(gRef.current).call(axis);
+    }, []);
+
     return(
         <g transform={`translate(${x}, ${y})`} />
     );
+
 };
 
 export default Axis;
