ea9a1d98aa17a36deb008405d730a0ebffeb7386
Martin <marb61a@gmail.com>
2020-10-13 20:13:17 +0100

    Updating scatterplot to use React hooks

--- scatterplot-app/src/App.js
+++ scatterplot-app/src/App.js
@@ -5,22 +5,22 @@ import * as d3 from "d3";
 import Scatterplot from "./Scatterplot"
 import Datapoint from './Datapoint';
 
-const data = d3
-  .range(100)
-  .map(_ => [
-    Math.random(),
-    Math.random()
-  ]);
+// const data = d3
+//   .range(100)
+//   .map(_ => [
+//     Math.random(),
+//     Math.random()
+//   ]);
 
-const data2 = d3
-  .range(100)
-  .map(_ => [
-    Math.random(),
-    Math.random()
-  ]);
+// const data2 = d3
+//   .range(100)
+//   .map(_ => [
+//     Math.random(),
+//     Math.random()
+//   ]);
 
 const App = () => {
-  const [ dimension, setDimensions ] = useState({
+  const [ dimensions, setDimensions ] = useState({
     width: 300,
     height: 300
   });
@@ -30,6 +30,14 @@ const App = () => {
       .map(_ => [Math.random(), Math.random()])
   );
 
+  function onClick(){
+    setDimensions({
+      width: dimensions.width * 0.7,
+      height: dimensions.height * 0.7
+    });
+
+  }
+
   return (
     <div className="App">
       <svg width="800" height="800" onClick={onClick}> 
@@ -37,58 +45,21 @@ const App = () => {
           x={50} 
           y={50} 
           data={data} 
-          width={this.state.width} 
-          height={this.state.height}
+          width={dimensions.width} 
+          height={dimensions.height}
           datapoint={({ x, y}) => <Datapoint x={x} y={y} />}
         />
         <Scatterplot 
           x={50} 
           y={450} 
           data={data} 
-          width={300} 
-          height={200} 
+          width={dimensions.width} 
+          height={dimensions.height} 
           datapoint={({ x, y}) => <circle cx={x} cy={y} r="5" />}
         />
       </svg>
     </div>
   );
-}  
-
-class App extends React.Component{
-  state = {
-    width: 300,
-    height: 200
-  };
-
-  onClick = () => this.setState({
-    width: this.state.width * 0.8,
-    height: this.state.height * 0.8
-  });
-
-  render(){
-    return (
-      <div className="App">
-        <svg width="800" height="800" onClick={this.onClick}> 
-          <Scatterplot 
-            x={50} 
-            y={50} 
-            data={data} 
-            width={this.state.width} 
-            height={this.state.height}
-            datapoint={({ x, y}) => <Datapoint x={x} y={y} />}
-          />
-          <Scatterplot 
-            x={50} 
-            y={450} 
-            data={data} 
-            width={300} 
-            height={200} 
-            datapoint={({ x, y}) => <circle cx={x} cy={y} r="5" />}
-          />
-        </svg>
-      </div>
-    );
-  }
-}
+};
 
 export default App;
