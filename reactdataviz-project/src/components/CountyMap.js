b40cecb7fa255a96f9e9c11410991d833fcc397b
Martin <marb61a@gmail.com>
2020-10-20 12:17:04 +0100

    Updated CountyMap functionality to render and center maps

--- reactdataviz-project/src/components/CountyMap.js
+++ reactdataviz-project/src/components/CountyMap.js
@@ -12,7 +12,32 @@ const CountyMap = ({
     height,
     zomm
 }) => {
+    const projection = d3
+        .geoAlbersUsa()
+        .scale(1280)
+        .translate([width / 2, height / 2])
+        .scale(width * 1.3);
+    const geoPath = d3.geoPath().projection(projection);
+    const quantize = d3.scaleQuantize().range(d3.range(9));
 
+    if(zoom && usTopoJson){
+        const us = usTopoJson;
+        const USstatePaths = topojson.feature(us, us.objects.states).features;
+        const id = _.find(USstateNames, { code: zoom}).id;
+
+        projection.scale(width * 4.5);
+        const centroid = geoPath.centroid(_.find(USstatePaths, { id: id}));
+        const translate = projection.translate();
+        
+    }
+
+    if(!usTopoJson){
+        return null;
+    } else {
+        return(
+            <div></div>
+        )
+    }
 }
 
 export default CountyMap
