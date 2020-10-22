7a46ce8012c4db9c8ebb10c5825275d6fb010dbb
Martin <marb61a@gmail.com>
2020-10-22 12:00:13 +0100

    Updating meta component functionality

--- reactdataviz-project/src/components/Meta.js
+++ reactdataviz-project/src/components/Meta.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { scaleLinear } from 'd3-scale';
+import { mean as d3mean, extent as d3extent} from 'd3-array';
+
+import USStatesMap from '../USstatesMap';
 
 export const Title = ({
     filteredSalaries, filteredBy
@@ -11,6 +15,29 @@ export const Title = ({
     function USstateFragment(){
         const USstate = filteredBy.USstate;
         return USstate ===  "*" ? "" : USStatesMap[USstate.toUpperCase()];
+    }
+
+    return function format(){
+        return scaleLinear
+            .domain(d3extent(data, (d) => d.base_salary))
+            .tickFormat()
+    }
+
+    function jobTitleFormat(){
+        const { jobTitle, year } = filteredBy;
+        let title = "";
+
+        if(jobTitle === "*"){
+            if(year === "*"){
+
+            } else {
+                
+            }
+        } else {
+
+        }
+
+        return title;
     }
 
 }
