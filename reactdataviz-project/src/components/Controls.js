c40b7a38ff938785dab132e7792518d8583ff9fe
Martin <marb61a@gmail.com>
2020-10-25 12:05:32 +0000

    Updating Controls.js component

--- reactdataviz-project/src/components/Controls.js
+++ reactdataviz-project/src/components/Controls.js
@@ -1,21 +1,39 @@
 import React, { useState, useEffect } from "react";
 
-const Controls = ({}) => {
-    const [ yearFilter, setYearFilter ] = useState(() => true);
-    const [ year, setYear ] = us("*");
+const Controls = ({ data, updateDataFilter }) => {
+    const [ filteredBy, setFilteredBy ] = useState({year: "*"});
+    const [ filter, setFilter ] = useState({ year: () => true});
 
-    const updateFilter = (year, reset) => {
-        let filter = (d) => d.submit_date.getFullYear() === year;
+    function reportUpdateUpTheChain(){
+        updateDataFilter();
+    }
+
+    const updateYearFilter = (year, reset) => {
+        let yearFilter = (d) => d.submit_date.getFullYear() === year;
 
         if(reset || !year){
-            filter = () => true;
+            yearFilter = () => true;
             year = "w";
         }
+
+        setFilteredBy((filteredBy) => {
+            return { ...filteredBy, year };
+        });
+        setFilter((filter) => {
+            return { ...filter, year: yearFilter}
+        });
     }
+
+    const years = new Set(data.map((d) => d.submit_date.getFullYear()));
 
     return(
         <div>
-            
+            <ControlRow 
+                data={data}
+                toggleNames={Array.from(years.values())}
+                picked={filteredBy.year}
+                updateYearFilter={updateYearFilter}
+            />
         </div>
     );
 
