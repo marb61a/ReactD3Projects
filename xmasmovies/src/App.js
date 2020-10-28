2e28ddb621236da57a411eddee7a68531cd174a8
Martin <marb61a@gmail.com>
2020-10-28 12:24:43 +0000

    Updated App.js with useEffect

--- xmasmovies/src/App.js
+++ xmasmovies/src/App.js
@@ -8,9 +8,27 @@ import "./styles.css";
 function App() {
   const [ data, setData ] = useState(null);
 
+  useEffect(() => {
+    d3.tsv("./public/data.tsv", d => {
+      const year = Number(d.movie.match(/\((\d+)\)/)[1]);
+
+      return{
+        movie: d.movie.replace(/\(\d+\)/, ""),
+        year: year,
+        per_year: Number(d.box_office) / (2018 - year),
+        box_office: Number(d.box_office)
+      }
+    }, [!data]);
+  });
+
   return (
     <div className="App">
       <h1>Christmas Movies at the box office</h1>
+      <p>
+        Home Alone is the highest grossing Christmas movie of all time. But does
+        the story change when you compare revenue per year? 🧐
+      </p>
+      
     </div>
   );
 }
