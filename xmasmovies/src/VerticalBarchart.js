8a423dd9b8bee31b9c0eacd1044a4d21eeaccbd6
Martin <marb61a@gmail.com>
2020-10-28 18:51:09 +0000

    Added basic VerticalBarchart component

---
+++ xmasmovies/src/VerticalBarchart.js
@@ -0,0 +1,23 @@
+import React from "react";
+import * as d3 from "d3";
+import styled from "styled-components";
+
+const Bar = styled.rect`
+    fill: green;
+    transition: width 500 ms;
+`;
+
+const Label = styled.text`
+    fill: white;
+    font-size: 11px;
+    alignment-baseline: central;
+`;
+
+const VerticalBarchart = () => {
+    return(
+        <g>
+            
+        </g>
+    );
+
+}
