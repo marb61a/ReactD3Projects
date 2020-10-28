e40d8211e2e7158f6d255ecbccc1818c1ca9cb58
Martin <marb61a@gmail.com>
2020-10-28 18:56:37 +0000

    Update VerticalBarchart component

--- xmasmovies/src/VerticalBarchart.js
+++ xmasmovies/src/VerticalBarchart.js
@@ -13,11 +13,33 @@ const Label = styled.text`
     alignment-baseline: central;
 `;
 
-const VerticalBarchart = () => {
+const VerticalBarchart = ({data, width, height, value}) => {
+    const yScale = d3
+        .scaleBand()
+        .paddingInner(0.1)
+        .domain(data.map(d => d.movie))
+        .range([0, height])
+    
+    const widthScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(data, value)])
+        .range([0, width]);
+
     return(
         <g>
-            
+            {data.map(d => (
+                <React.Fragment>
+                    <Bar 
+                        x={0}
+                        y={yScale(d.movie)}
+                        height={yScale.bandwidth()}
+                        width={widthScale(value(d))}
+                    />
+                </React.Fragment>
+            ))}
         </g>
     );
 
 }
+
+export default VerticalBarchart;
