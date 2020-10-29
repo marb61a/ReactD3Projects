5a38ea46ccd79d579f28dc188b75e2a53c6d81c1
Martin <marb61a@gmail.com>
2020-10-29 19:29:01 +0000

    Updated app.js

--- xmasmovies/src/App.js
+++ xmasmovies/src/App.js
@@ -4,6 +4,20 @@ import * as d3 from "d3";
 import styled from "styled-components";
 
 import "./styles.css";
+import VerticalBarchart from "./VerticalBarchart";
+
+const Button = styled.button`
+  cursor: pointer;
+  margin-bottom: 1em;
+`;
+
+const Svg = styled.svg`
+  background: ${({ showKevin }) =>
+    showKevin
+      ? "url(https://media.giphy.com/media/d2ZjBlsQa5dWO45a/giphy.gif) no-repeat center center"
+      : ""};
+  background-size: cover;
+`;
 
 function App() {
   const [ data, setData ] = useState(null);
@@ -21,6 +35,9 @@ function App() {
     }, [!data]);
   });
 
+  const [perYear, setPerYear] = useState(false);
+  const valueFunction = perYear ? d => d.per_year : d => d.box_office;
+
   return (
     <div className="App">
       <h1>Christmas Movies at the box office</h1>
@@ -28,7 +45,19 @@ function App() {
         Home Alone is the highest grossing Christmas movie of all time. But does
         the story change when you compare revenue per year? 🧐
       </p>
-      
+      <Button onClick={() => setPerYear(!perYear)}>
+        {perYear ? "Show Total Box Office" : "Show Box Office Per Year"}
+      </Button>
+      <Svg width="800" height="600" showKevin={perYear}>
+        {data && (
+          <VerticalBarchart
+            data={data}
+            width={600}
+            height={600}
+            value={valueFunction}
+          />
+        )}
+      </Svg>
     </div>
   );
 }
