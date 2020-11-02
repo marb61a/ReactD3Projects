ab05178ba0f6966fd64a312870319d6e6fd44b10
Martin <marb61a@gmail.com>
2020-11-02 19:51:33 +0000

    added dependencies and updated app.js

--- mooreslawapp/src/App.js
+++ mooreslawapp/src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import * as d3 from "d3";
+import faker from "faker";
+
+const Svg = styled.svg`
+    width: 100vw;
+    height: 100vh;
+    background: #0b0c10;
+`;
 
 function App() {
+  const [data, setData] = useState(null);
+  const yearIndex = d3
+    .scaleLinear()
+    .domain([1970, 2025])
+    .range([0, 2025 - 1970]);
+
   return (
     <div className="App">
       <header className="App-header">
