1ef9e740ab29bef441ca0b4dc6d911dd0df2319c
Martin <marb61a@gmail.com>
2020-11-03 11:56:40 +0000

    Updated Barchart.js with Bar function and styling

--- mooreslawapp/src/Barchart.js
+++ mooreslawapp/src/Barchart.js
@@ -2,11 +2,34 @@ import React from "react";
 import styled from "styled-components";
 import * as d3 from "d3";
 
-const Bar = () => {
+const label = styled.text`
+    fill: white;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+        "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+        "Helvetica Neue", sans-serif;
+    font-size: 14px;
+    text-anchor: end;
+    alignment-baseline: middle;
+`;
 
+const Bar = ({ data, y, width, thickness}) => {
+    return(
+        <g transform={`translate(${0}, ${y})`}>
+            <rect 
+                x={10}
+                y={0}
+                width={width}
+                height={thickness}
+                fill="white"
+            />
+            <label >
+                {data.name}
+            </label>
+        </g>
+    )
 }
 
-const Barchart = ({ data, x, y, barWidth}) => {
+const Barchart = ({ data, x, y, barThickness, width}) => {
     const yScale = d3
         .scaleBand()
         .domain(data.map(d => d.name))
