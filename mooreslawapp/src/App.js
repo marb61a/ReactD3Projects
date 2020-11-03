6d458c39bf4884620cf7c0f9842ad3a02b99066c
Martin <marb61a@gmail.com>
2020-11-03 12:12:21 +0000

    Updated App.js app function and useEffect to set interval for rerendering

--- mooreslawapp/src/App.js
+++ mooreslawapp/src/App.js
@@ -11,8 +11,23 @@ const Svg = styled.svg`
     background: #0b0c10;
 `;
 
+const Year = styled.text`
+    fill: white;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+        "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+        "Helvetica Neue", sans-serif;
+    font-size: 120px;
+    font-weight: bold;
+    text-anchor: end;
+`;
+
+const useData = () => {
+
+};
+
 function App() {
-  const [data, setData] = useState(null);
+  const data = useData(); 
+  const [currentYear, setCurrentYear] = useState(1970);
   const yearIndex = d3
     .scaleLinear()
     .domain([1970, 2025])
@@ -22,15 +37,27 @@ function App() {
   // D3 timers are more intelligent
   useEffect(() => {
     const interval = d3.interval(() => {
+      console.log("Interval Ran!");
 
-    })
+      if(currentYear > 2025){
+        interval.stop();
+      }
+
+      setCurrentYear(year => year + 1);
+    }, 1000);
+
+    return () => interval.stop();
   }, []);
 
   return (
     <svg>
       {data ? (
         <Barchart 
-        
+          data={data[0]}
+          x={100}
+          y={50}
+          barThickness={20}
+          width={500}
         />
       ) : null}
     </svg>
