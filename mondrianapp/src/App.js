dfad582fe0a42f6b0dc1afd51a64189d3cddabc2
Martin <marb61a@gmail.com>
2020-11-10 11:58:41 +0000

    updated App.js and added basic Mondrian file

--- mondrianapp/src/App.js
+++ mondrianapp/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+
+import Mondrian from './Mondrian';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Piet Mondrian Art Generator</h1>
+      <svg
+        width="100%"
+        height="100%"
+        style={{border: "1px solid red"}}
+      >
+      </svg>
     </div>
   );
 }
---
+++ mondrianapp/src/Mondrian.js
@@ -0,0 +1,9 @@
+import React from "react";
+import * as d3 from "d3";
+import { hierarchy } from "d3-hierarchy";
+
+const Mondrian = ({ width, height }) => {
+
+};
+
+export default Mondrian;
