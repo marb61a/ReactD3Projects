96451d31c0389477173f53d4ee1471eaab96dbc3
Martin <marb61a@gmail.com>
2020-11-11 11:57:07 +0000

    Updated app.js with Range function

--- mondrianapp/src/App.js
+++ mondrianapp/src/App.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import * as d3 from "d3";
 import './App.css';
 
 import Mondrian from './Mondrian';
+import useMondrianGenerator from "./useMondrianGenerator";
+
+const Range = ({ name, value, onChnage }) => {
+  return(
+    <div style={{ display: "inline-block" }}>
+      {name}
+      <br />
+      <input
+        type="range"
+        name={name}
+        min={0}
+        max={1}
+        step={0.1}
+        value={value}
+        onChange={event => onChange(Number(event.target.value))}
+      />      
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Piet Mondrian Art Generator</h1>
+      <header>
+        <h1>Piet Mondrian Art Generator</h1>
+        
+      </header>
       <svg
         width="100%"
         height="100%"
