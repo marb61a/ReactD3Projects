8d19599ad8f051e8f2e78fc52a0b8dc1a6c0591e
Martin <marb61a@gmail.com>
2020-11-11 12:12:39 +0000

    Added MondrainRectangle function to Mondrian.js

--- mondrianapp/src/Mondrian.js
+++ mondrianapp/src/Mondrian.js
@@ -2,6 +2,30 @@ import React from "react";
 import * as d3 from "d3";
 import { hierarchy } from "d3-hierarchy";
 
+const MondrianReactangle = ({ node}) => {
+    const { x0, y0, x1, y1, children } = node;
+    const width = x1 - x0;
+    const height = y1 - y0;
+
+    return(
+        // React Fragement
+        <>
+            <rect 
+                x={x0}
+                y={y0}
+                width={width}
+                height={height}
+                style={{
+                    fill: node.data.color,
+                    stroke: black,
+                    strokeWidth: 5
+                }}
+            />
+        </>
+    );
+
+};
+
 const Mondrian = ({ x, y, width, height }) => {
     return(
         <g transform={`translate(${x}, ${y})`}>
